7c464ebbc6c824c124d962235fad24cfccc41943
Remax Cony <kevin.espino@remax.es>
2024-08-20 14:33:58 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,313 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.849983594354976,
+        "pitch": 0.013898748131175864,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8068906396425586,
+          "pitch": 0.046678698206289226,
+          "rotation": 0,
+          "target": "1-pasillo"
+        },
+        {
+          "yaw": 1.6063917164033494,
+          "pitch": 0.09981902593411007,
+          "rotation": 0,
+          "target": "6-dormitorio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pasillo",
+      "name": "Pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.8229714514143076,
+        "pitch": 0.16484064322250092,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05900614341392796,
+          "pitch": 0.12077235014551135,
+          "rotation": 0,
+          "target": "2-bano"
+        },
+        {
+          "yaw": -1.6508826265270375,
+          "pitch": 0.1925692520512765,
+          "rotation": 0,
+          "target": "3-salon"
+        },
+        {
+          "yaw": -2.0924762666018495,
+          "pitch": 0.1890031254048541,
+          "rotation": 0,
+          "target": "4-dormitorio-2"
+        },
+        {
+          "yaw": 1.4281207434501741,
+          "pitch": 0.022603086893941082,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bano",
+      "name": "Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.491432167223639,
+        "pitch": 0.12803070126295069,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7372659196574354,
+          "pitch": 0.09410125791871593,
+          "rotation": 0,
+          "target": "1-pasillo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salon",
+      "name": "Salón",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5040506582246378,
+        "pitch": -0.02372969188894558,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7368351297171287,
+          "pitch": 0.1723611246596395,
+          "rotation": 0,
+          "target": "1-pasillo"
+        },
+        {
+          "yaw": 1.4897219679492082,
+          "pitch": 0.016453967817156467,
+          "rotation": 0,
+          "target": "5-salon-2"
+        },
+        {
+          "yaw": -0.644969350604006,
+          "pitch": 0.05009883784673619,
+          "rotation": 0,
+          "target": "4-dormitorio-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dormitorio-2",
+      "name": "Dormitorio 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5573385242882178,
+        "pitch": -0.0786490386620251,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.338981634974134,
+          "pitch": 0.23528837664076896,
+          "rotation": 0,
+          "target": "3-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salon-2",
+      "name": "Salón 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.0076223181918742,
+        "pitch": -0.04751126104293135,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9592614163047877,
+          "pitch": 0.12287590784715974,
+          "rotation": 0,
+          "target": "3-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dormitorio",
+      "name": "Dormitorio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.212923735174301,
+        "pitch": 0.08973524224233032,
+        "fov": 1.289724167914279
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9155075390030518,
+          "pitch": 0.15143746678536374,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
